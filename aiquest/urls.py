851886fb264eeb7dc0e6011ea4a68dc66e99e193
Shhaammiimm <shamim20517@gmail.com>
2026-09-08 01:31:43 +0600

    Added a new app deep learning. set up relation between views and urls.

--- aiquest/urls.py
+++ aiquest/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from machine_learning import views
+from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.machine)
+    path('machine/', include('machine_learning.urls')),
+    path('deep/', include('deep_learning.urls'))
 ]
--- deep_learning/views.py
+++ deep_learning/views.py
@@ -1,10 +1,11 @@
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, this is the machine learning view.")
+    return HttpResponse("Hello, this is the deep learning view.")
 
 def say_hello(request):
-    return HttpResponse("Hello from the machine learning app!")
+    return HttpResponse("Hello from the deep learning app!")
